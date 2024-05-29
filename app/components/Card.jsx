0024744a388f3519dff88b492b2fd832a3f9896a
Don Tobias <dtobiasalmont09@gmail.com>
2024-05-29 09:11:33 -0400

    Created a basic next.js app with daisyui and tailwind

---
+++ app/components/Card.jsx
@@ -0,0 +1,12 @@
+export default Card;
+
+function Card() {
+    return (
+        <div className="card w-96 bg-base-100 shadow-xl m-4">
+            <div className="card-body items-center text-center">
+                <p>Todo Item</p>
+            </div>
+        </div>
+    )
+}
+
---
+++ app/components/Header.tsx
@@ -0,0 +1,9 @@
+export default Header
+
+function Header() {
+    return (
+        <header className="border-gray-200 dark:bg-gray-900 flex flex-wrap p-4 justify-center">
+            <h1 className="text-stone-50 text-3xl">Todo List</h1>
+        </header>
+    )
+}
