fa697cbc38d5d2cf9f6ea815e6f49ffc9ff120ec
Don Tobias <dtobiasalmont09@gmail.com>
2024-06-03 08:50:19 -0400

    Attempt at getting modal to work

--- app/components/AddItemButton.tsx
+++ app/components/AddItemButton.tsx
@@ -1,11 +1,29 @@
+'use client'
+import React, { useState } from 'react';
+import Modal from "./Modal";
+
 export default AddItemButton
 
-function AddItemButton() {
+type AddItemButtonProps = {
+    createTypeName: string;
+}
+
+function AddItemButton({ createTypeName }: AddItemButtonProps) {
+    const [isModalOpen, setIsModalOpen] = useState(true);
+
+    const handleButtonClick = () => {
+        setIsModalOpen(true);
+    };
+
     return (
-        <button className="btn btn-circle rounded-full w-12 h-12 flex items-center justify-center"
-            title="Create new item"
-            aria-label="Create new item">
-            <span className="text-2xl">+</span>
-        </button>
+        <>
+            {/* <button className="btn btn-circle rounded-full w-12 h-12 flex items-center justify-center"
+                title={`Create ${createTypeName} item`}
+                aria-label={`Create ${createTypeName} item`}
+                onClick={handleButtonClick}>
+                <span className="text-2xl">+</span>
+            </button > */}
+            <Modal />
+        </>
     )
 }
